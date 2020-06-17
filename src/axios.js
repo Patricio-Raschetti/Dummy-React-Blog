829d7092bbb58f23c7bdbcb41fc911fb63ccd169
Patricio-Raschetti <patricio.raschetti@gmail.com>
2020-06-16 22:42:15 -0300

    testing github

--- src/axios.js
+++ src/axios.js
@@ -4,6 +4,7 @@ const instance = axios.create({
     baseURL: 'https://jsonplaceholder.typicode.com'
 });
 
+
 instance.defaults.headers['Authorization'] = 'Bearer AUTH TOKEN FROM INSTANCE';
 
 export default instance;
