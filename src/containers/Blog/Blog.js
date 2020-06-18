91e2780f58f4bb021fba6d8485a713bcc6b44a43
Patricio-Raschetti <patricio.raschetti@gmail.com>
2020-06-18 00:10:44 -0300

    Refactoring views Components into Containers

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,47 +1,12 @@
 import React, { Component } from 'react';
-// Using 'axios' identifier instead of 'axiosBlogInstance' or whatever more specific for not changing the code.
-// import axios from 'axios';
-import axios from '../../axios';
-
+import Posts from '../Posts/Posts';
 import FullPost from '../FullPost/FullPost';
 import NewPost from '../NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPost: null,
-        error: false
-    };
-
-    async componentDidMount() {
-        try {
-            const response = await axios.get('/posts/');
-            const posts = response.data.slice(0, 4);
-            const updatedPosts = posts.map(post => ({ ...post, author: 'Pato' }));
-            this.setState({ posts: updatedPosts });
-        } catch (error) {
-            this.setState({ error: true });
-        };
-    };
-
-    selectPostHandler = id => {
-        this.setState({ selectedPost: id });
-    };
 
     render() {
-        let posts = <p style={{ textAlign: "center" }}>Couldn't fetch the post. Try again later!</p>
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post
-                    key={post.id}
-                    title={post.title}
-                    author={post.author}
-                    clicked={this.selectPostHandler.bind(this, post.id)}
-                />
-            });
-        };
-
         return (
             <div className="Blog">
                 <header>
@@ -52,18 +17,10 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPost} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+                <Posts />
             </div>
         );
-    }
-}
+    };
+};
 
 export default Blog;
--- src/containers/Posts/Posts.js
+++ src/containers/Posts/Posts.js
@@ -1,2 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Post from '../../components/Post/Post';
+import axios from '../../axios';    // Using 'axios' identifier instead of 'axiosBlogInstance' or whatever more specific for not changing the code.
+import './Posts.css';
+
+class Posts extends Component {
+    state = {
+        posts: []
+        // selectedPost: null,
+        // error: false
+    };
+
+    async componentDidMount() {
+        try {
+            const response = await axios.get('/posts/');
+            const posts = response.data.slice(0, 4);
+            const updatedPosts = posts.map(post => ({ ...post, author: 'Pato' }));
+            this.setState({ posts: updatedPosts });
+        } catch (error) {
+            console.log(error);
+            // this.setState({ error: true });
+        };
+    };
+
+    selectPostHandler = id => {
+        this.setState({ selectedPost: id });
+    };
+
+    render() {
+        let posts = <p style={{ textAlign: "center" }}>Couldn't fetch the post. Try again later!</p>
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={this.selectPostHandler.bind(this, post.id)}
+                />
+            });
+        };
+
+        return (
+            <section className="Posts">
+                {posts}
+            </section>
+        );
+    };
+};
+
+export default Posts;
+
+/* 
+
+
+*/
